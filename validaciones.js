1e27b61f29713a4560dfb0a7f3023efa84e4393a
martinagarcia05 <114435019+martinagarcia05@users.noreply.github.com>
2025-06-04 19:46:37 -0300

    ultimas mods

--- validaciones.js
+++ validaciones.js
@@ -1,9 +1,9 @@
 function () {
   const formulario = document.getElementById("formulario");
 
-    const nom = document.getElementById("nombre").value.trim();
-    const ap1 = document.getElementById("apellido1").value.trim();
-    const ap2 = document.getElementById("apellido2").value.trim(); 
+    const nom = document.getElementById("nombre").value;
+    const ap1 = document.getElementById("apellido1").value;
+    const ap2 = document.getElementById("apellido2").value;
 
     if (nom === "") {
       alert("Ingrese su nombre");
